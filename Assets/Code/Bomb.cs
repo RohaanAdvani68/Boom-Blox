5f5d61fb426150577c16a289f6412551c1be5eae
Rohaan Advani <RohaanAdvani68@gmail.com>
2016-11-16 14:24:07 -0600

    first commit
    Signed-off-by: Rohaan Advani <RohaanAdvani68@gmail.com>

---
+++ Assets/Code/Bomb.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+    public float ThresholdForce = 2;
+    public GameObject ExplosionPrefab;
+
+	void Destruct(){
+		GameObject.Destroy (gameObject);
+	}
+
+	void Boom(){
+		PointEffector2D obj = gameObject.GetComponent<PointEffector2D> ();
+		obj.enabled = true;
+		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer> ();
+		sr.enabled = false;
+		Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+		Invoke ("Destruct", 0.1f);
+	}
+
+	internal void OnCollisionEnter2D(Collision2D obj){
+		if (obj.relativeVelocity.magnitude >= ThresholdForce)
+			Boom ();
+	}
+}
---
+++ Assets/Code/TargetBox.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TargetBox : MonoBehaviour
+{
+    /// <summary>
+    /// Targets that move past this point score automatically.
+    /// </summary>
+    public static float OffScreen;
+
+    internal void Start() {
+        OffScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-100, 0, 0)).x;
+    }
+
+    internal void Update()
+    {
+        if (transform.position.x > OffScreen)
+            Scored();
+    }
+
+    private void Scored()
+    {
+		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer> ();
+		float m = gameObject.GetComponent<Rigidbody2D> ().mass;
+		if (sr.color != Color.green) {
+			ScoreKeeper.AddToScore (m);
+			sr.color = Color.green;
+		}
+    }
+
+	internal void OnCollisionEnter2D(Collision2D obj){
+		if (obj.gameObject.tag == "Ground")
+			Scored ();
+	}
+}
